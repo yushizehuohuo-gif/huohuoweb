11f26ab73a626e9ff3af56536cff9b21ea82c9ee
HuoHuoOvO <yushizehuohuo@gmail.com>
2026-07-20 10:02:12 +0800

    deploy: huohuoweb v2 - fonts, clean code, sitemap, no-JS fallback

---
+++ _next/static/j4f-LC1pU_c2-BWUuu6S-/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/huohuoweb//_next/:path+",
+        "destination": "/huohuoweb/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
